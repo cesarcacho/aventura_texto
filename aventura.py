1be295012bf9964f2348d8aaeca0ff2b382423e7
cesarcacho <cesarcacho@gmail.com>
2022-03-30 23:24:06 +0200

    todas las salas

--- aventura.py
+++ aventura.py
@@ -3,6 +3,7 @@
 import time
 import os
 
+jugador = {}
 
 def clearConsole():
     command = 'clear'
@@ -57,7 +58,7 @@ def sala4 ():
     if direc == "E":
         sala5()
     elif direc == "N":
-        print("Subiria a la sala 7")
+        sala7()
     elif direc == "S":
         sala2()
 
@@ -85,6 +86,42 @@ def sala6 ():
     elif direc == "S":
         sala3()
 
+def sala7 ():
+    clearConsole()
+    print("Estás en la sala 7")
+    print("Ves puertas en el Este y Sur ")
+    print("¿ En que dirección quieres ir ?")
+    direc = input((" E o S"))
+    if direc == "E":
+        sala8()
+    elif direc == "S":
+        sala4()
+
+def sala8 ():
+    clearConsole()
+    print("Estás en la sala 8")
+    print("Ves puertas en el Oeste, Norte y Este ")
+    print("¿ En que dirección quieres ir ?")
+    direc = input((" O, N o E"))
+    if direc == "E":
+        sala9()
+    elif direc == "O":
+        sala7()
+    elif direc == "":
+        print("Sala del foso")
+
+def sala9 ():
+    clearConsole()
+    print("Estás en la sala 9")
+    print("Ves puertas en el Oste y Norte ")
+    print("¿ En que dirección quieres ir ?")
+    direc = input((" O o N"))
+    if direc == "O":
+        sala8()
+    elif direc == "N":
+        print("Sala del BOSS final")
+
+
 def entrada():
     clearConsole()
     print("Estás dentro")
@@ -102,42 +139,59 @@ def entrada():
     elif direc == "S":
         print("Cobarde!!!!! Juuuurrrllll")
 
-
+def eleccion_jugador(respuesta):
+    if respuesta == "Alex":
+        jugador["nombre","Alex"]
+        jugador["vida",5]
+        jugador["fuerza",10]
+        jugador["escudo",10]
+    elif respuesta == "Cesar":
+        jugador["nombre","Cesar"]
+        jugador["vida",5]
+        jugador["fuerza",12]
+        jugador["escudo",8]
+    elif respuesta == "Marta":
+        jugador["nombre","Martis"]
+        jugador["vida",5]
+        jugador["fuerza",12]
+        jugador["escudo",8]
 
 clearConsole()
 
-for num in range (10):
-    time.sleep(0.5)
-    clearConsole()
-    if num % 2 == 0:
-        print("The quest for directasa, The Videojocco")
-        print("     X X    ")
-        print("      X     ")
-        print("     X X    ")
-    else:
-        print("The quest for directasa, The Videojocco")
-        print("      X     ")
-        print("     X X    ")
-        print("      X     ")
+# for num in range (10):
+#     time.sleep(0.5)
+#     clearConsole()
+#     if num % 2 == 0:
+#         print("The quest for directasa, The Videojocco")
+#         print("     X X    ")
+#         print("      X     ")
+#         print("     X X    ")
+#     else:
+#         print("The quest for directasa, The Videojocco")
+#         print("      X     ")
+#         print("     X X    ")
+#         print("      X     ")
 
 
-for num in range (5):
-    
-    bolita = "x"
+for num in range (3):
+    bolita = "******"
     espacio = " "
-    for desliz in range (25):
+    for desliz in range (30):
         print("The quest for directasa, The Videojocco")
         if desliz == 0:
             mostrar = espacio + bolita 
         else:
             mostrar = espacio + mostrar
         print(mostrar)
-        time.sleep(0.1)
+        time.sleep(0.05)
         clearConsole()
 
 print("La DIRECTASA LA ROBÓ UN MANDRIL LUNAR")
 print("La ha escondido en el fondo de una mazmorra")
 print("¿ Quieres ir a buscarla ?")
+print("¿Que personaje quieres ser?")
+respuesta = input("Alex, Cesar o Marta")
+eleccion_jugador(respuesta)
 
 respuesta = input(("si/no " ))
 if respuesta == "S":
